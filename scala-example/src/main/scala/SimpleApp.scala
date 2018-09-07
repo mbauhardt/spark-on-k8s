65f6ff96a9f03b2acc49eef2e48a1552c49695d2
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-09-07 21:13:25 +0200

    simple app scala expects an argument

--- scala-example/src/main/scala/SimpleApp.scala
+++ scala-example/src/main/scala/SimpleApp.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
 
   def main(args: Array[String]) {
-    val logFile = "/opt/spark/README.md" // Should be some file on your system
+    val logFile = args(0)//"/opt/spark/README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
